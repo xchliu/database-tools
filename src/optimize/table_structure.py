4ec99cd294d84290feb6d33ce65671f65ab040b5
Xchliu <wyliuxiaocheng@jd.com>
2016-02-25 18:31:59 +0800

    daily

---
+++ src/optimize/table_structure.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Author : xchliu
+    Date   : 2/25/16
+    
+"""
+
+
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
